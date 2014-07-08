f8ba8f8ce94e226fc796844a70e6d5de1ac595a3
jenca <lajencaku@gmail.com>
2014-07-08 16:19:26 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+$(window).ready(function(){
+    var pages = $('.show')
+    var current = 0;
+    var currentPage;
+    var nextPage;
+
+    $('.button').click(function(){
+        currentPage = pages.eq(current);
+        if($(this).hasClass('previous')){
+            if (current <= 0)
+                current = pages.length-1;
+            else
+                current = current-1;
+        }
+         else
+        {
+            if (current >= pages.length-1)
+                current = pages.length-1;
+            else
+                current = current+1;
+        }
+
+
+        nextPage = pages.eq(current);
+        currentPage.hide();
+        nextPage.show();
+    });
+});
+
+$(document).ready(function() {
+    $.scrollify ({
+        section : ".panel"
+    })
+});
+},{}]},{},[1])
